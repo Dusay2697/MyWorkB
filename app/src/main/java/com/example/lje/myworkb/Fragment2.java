c3e5e6018a9c3fa60148d46efc00e46de9d103a9
이정은 <dlwjddmsiove@naver.com>
2017-11-06 21:28:33 +0900

    Update Fragment2.java

--- app/src/main/java/com/example/lje/myworkb/Fragment2.java
+++ app/src/main/java/com/example/lje/myworkb/Fragment2.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 
 
 public class Fragment2 extends Fragment {
+    private Toast toast;
+    
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -17,5 +19,20 @@ public class Fragment2 extends Fragment {
         return inflater.inflate(R.layout.activity_fragment2,container,false);//인플레이터:XML에서 만든 화면을 단말기에서 볼수있게 해주는 메소드
 
 
+    }
+    //Toast
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.buttonShow: {
+                toast.show();
+                break;
+
+            }
+            case R.id.buttonCancel: {
+                toast.cancel();
+                break;
+            }
+        }
     }
 }
